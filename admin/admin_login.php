7631b0eadedde8c81d21b4cc8ab102126850d6df
Nafizul <nafizul35-480@diu.edu.bd>
2023-10-11 00:22:26 +0600

    admin customer page update

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Admin - Login| ShosurBari</title>
+    <title>Admin - Login | ShosurBari</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon
--- admin/contact_us.php
+++ admin/contact_us.php
@@ -20,7 +20,7 @@ if (!isset($_SESSION['id'])) {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Admin | ShosurBari</title>
+    <title>Admin - ContactUs| ShosurBari</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon
