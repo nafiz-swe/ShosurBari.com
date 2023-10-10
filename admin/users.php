b81ded589019989581a888f3b59cc9ec20ba4347
Nafizul <nafizul35-480@diu.edu.bd>
2023-10-11 00:16:43 +0600

    admin register update

--- admin/users.php
+++ admin/users.php
@@ -24,7 +24,7 @@ if (!isset($_SESSION['id'])) {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Admin | ShosurBari</title>
+    <title>Admin - Users | ShosurBari</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon
