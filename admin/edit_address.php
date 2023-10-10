fcb72fec822f484c8023a93ac2e2f106d9935192
Nafizul <nafizul35-480@diu.edu.bd>
2023-10-11 00:40:58 +0600

    admin update education modify

--- admin/edit_address.php
+++ admin/edit_address.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION['id'])) {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Admin - Update Address| ShosurBari</title>
+    <title>Admin - Update Address | ShosurBari</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon
