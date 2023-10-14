d58241133f358cea8159f8fbcfedbc474c9248c4
Nafizul <nafizul35-480@diu.edu.bd>
2023-10-14 23:26:44 +0600

    user profile picture positions move, css code change for user profile img

--- admin/index.php
+++ admin/index.php
@@ -879,7 +879,7 @@ if ($result) {
                 <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                     <div style="background: #07e27e2b;" class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">
                         <div class="website-traffic-ctn">
-                            <h2><?php echo $countValue3; ?></h2>
+                            <h2><span class="counter"><?php echo $countValue3; ?></span></h2>
                             <p>Package [3 SbBd 375 Tk]</p>
                         </div>
                         <div class="sparkline-bar-stats1">9,4,8,6,5,6,4,8,3,5,9,5</div>
@@ -889,7 +889,7 @@ if ($result) {
                 <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                     <div style="background: #07e27e2b;" class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">
                         <div class="website-traffic-ctn">
-                            <h2><?php echo $countValue4; ?></h2>
+                            <h2><span class="counter"><?php echo $countValue4; ?></span></h2>
                             <p>Package [4 SbBd 460 Tk]</p>
                         </div>
                         <div class="sparkline-bar-stats2">1,4,8,3,5,6,4,8,3,3,9,5</div>
@@ -908,7 +908,7 @@ if ($result) {
                 <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                     <div style="background: #07e27e2b;" class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30 dk-res-mg-t-30">
                         <div class="website-traffic-ctn">
-                            <h2><?php echo $countValue5; ?></h2>
+                            <h2><span class="counter"><?php echo $countValue5; ?></span></h2>
                             <p>Package [5 SbBd 525 Tk]</p>
                         </div>
                         <div class="sparkline-bar-stats3">4,2,8,2,5,6,3,8,3,5,9,5</div>
@@ -918,7 +918,7 @@ if ($result) {
                 <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                     <div style="background: #07e27e2b;" class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30 dk-res-mg-t-30">
                         <div class="website-traffic-ctn">
-                        <h2><?php echo $countValue10; ?></h2>
+                        <h2><span class="counter"><?php echo $countValue10; ?></span></h2>
                         <p>Package [10 SbBd 990 Tk]</p>
                         </div>
                         <div class="sparkline-bar-stats4">2,4,8,4,5,7,4,7,3,5,7,5</div>
