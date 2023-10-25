24d0eb9afbd814130634df49e80ea0c6ef7e57ca
Nafizul <nafizul35-480@diu.edu.bd>
2023-10-25 23:55:25 +0600

    home page search options margin change

--- index.php
+++ index.php
@@ -1164,6 +1164,7 @@ toggleLabels.forEach(label => {
   border: 2px solid #f0f0f0;
   border-radius: 0px 0px 10px 10px;
   box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+  margin-top: 90px;
 }
 
 .inner-wrap {
